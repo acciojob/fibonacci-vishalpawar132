e61f910568d45c18a6648997026639674f81659c
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-07-12 10:22:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	if(num === 0){
+		return 0;
+	}
+	else if(num === 1){
+		return 1 ;
+	}else {
+		const fb = [0, 1];
+		for(let i = 2;  i <= num; i++){
+			fib.push(fb[i-1] + fb[i-2]);
+		}
+		return fb[num;
+	}
 }
 
 module.exports = fibonacci;
