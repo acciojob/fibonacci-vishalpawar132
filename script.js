c79d411a974e4e05e9866e02bed60046d3b43bf6
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-07-22 18:09:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 function fibonacci(num) {
-  if (num === 0) return 0;
-  if (num === 1) return 1;
+	return fib1(num-1);
+}
 
-  // Use memoization to store previously computed Fibonacci numbers
-  const memo = new Map();
-  memo.set(0, 0);
-  memo.set(1, 1);
-
-  // Helper function to calculate the Fibonacci number
-  function fib(n) {
-    if (memo.has(n)) {
-      return memo.get(n);
-    } else {
-      const fibN = fib(n - 1) + fib(n - 2);
-      memo.set(n, fibN);
-      return fibN;
-    }
-  }
-
-  return fib(num);
-   
+function fib1(n){
+	if(n <= 0){
+		return 0;
+		return recursive(n-1, 0, 1);
+	}
+	function recursive(n, prev, curr){
+		if(n === 0)
+			return curr;
+		return recursive(n-1, curr, curr + prev);
+	}
 }
 
 module.exports = fibonacci;
